b3e9d30022ebe23de18d98bbe7bdb7201af4ff58
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2023-11-29 10:34:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,37 +27,11 @@ resource "snowflake_schema" "demo_schema_new" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA_new"
   comment  = "Schema for Snowflake Terraform demo"
-}
+}  
 
-resource "snowflake_file_format" "new_file_format" {
-  name          = "new_file_format"
-  database      = snowflake_database.demo_db.name
-  schema        = snowflake_schema.demo_schema_new.name
-  format_type   = "CSV"
-  field_option  = "FIXED"
-  record_delimiter = ","
-}
-
-resource "snowflake_table" "s3_integration" {
-  name     = "s3_integration"
-  database = snowflake_database.demo_db.name
-  schema   = snowflake_schema.demo_schema_new.name
-
-  column {
-    name    = "column1"
-    type    = "TEXT"
-  }
-
-  column {
-    name    = "column2"
-    type    = "NUMBER"
-  }
-
-  column {
-    name    = "column3"
-    type    = "VARCHAR(255)"
-  }
-
-  file_format = snowflake_file_format.new_file_format.name
-}
+resource "snowflake_file_format" "demo_file_format" {
+  database = snowflake_database.demo_db.demo_schema_new.name
+  name     = "demo_file_format"
+  comment  = "file format for Snowflake Terraform demo"
+}  
 
