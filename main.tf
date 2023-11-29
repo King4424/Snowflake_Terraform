cac38be9b3f04f86f021fed7aaff0728e1cee3a9
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2023-11-29 17:47:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,8 +61,8 @@ resource "snowflake_stage" "example_stage" {
 }
 
 resource "snowflake_stage_grant" "grant_example_stage" {
-  database_name = snowflake_stage.example_stage.database
-  schema_name   = snowflake_stage.example_stage.schema
+  database_name = snowflake_stage.demo_db.database
+  schema_name   = snowflake_stage.demo_schema.schema
   roles         = ["LOADER"]
   privilege     = "OWNERSHIP"
   stage_name    = snowflake_stage.example_stage.name
