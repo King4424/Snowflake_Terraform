8cc961ee4290d65b75a7bdfb329b53e8752b3bb7
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2023-11-29 11:54:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,29 +27,3 @@ resource "snowflake_schema" "demo_schema" {
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
-resource "snowflake_file_format" {
-  database = snowflake_database.demo_db.name
-  schema   = snowflake_schema.demo_schema.name
-  name     = "DEMO_FILE_FORMAT"
-  
-  type                = "CSV"
-  field_optionally_enclosed_by = '"'
-  compression         = "GZIP"
-  record_delimiter    = "\n"
-  field_delimiter     = ","
-  skip_header         = 1
-  date_format         = "AUTO"
-  time_format         = "AUTO"
-  timestamp_format    = "AUTO"
-  binary_format       = "BASE64"
-  escape              = "\\"
-  escape_unenclosed_field = "\\"
-  trim_space          = false
-  field_optionally_enclosed_by = ""
-  null_if             = ["NULL"]
-  error_on_column_count_mismatch = false
-  validate_utf8       = false
-  skip_byte_order_mark = false
-  encoding            = "UTF8"
-  comment             = "File Format for Snowflake Terraform demo"
-}
