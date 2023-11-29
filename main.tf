7cd13c45ec75b4ed256b5a369e67e249cbc5f263
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2023-11-29 16:46:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -58,5 +58,5 @@ resource "snowflake_stage" "snowstage" {
   url         = "s3://snowflake-nse-data/"
   database    = snowflake_database.demo_db.name
   schema      = snowflake_schema.demo_schema.name
-  file_format = "FORMAT_NAME = JSON"
+  file_format = "FORMAT_NAME = csv"
 }
