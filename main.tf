aa25e7b31951cff1c5e002a5ba87655590838311
Vaibhav Arun Khairnar <121480992+King4424@users.noreply.github.com>
2023-11-29 11:10:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,7 +22,6 @@ resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
-
 resource "snowflake_schema" "demo_schema" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA"
